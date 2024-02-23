0a6f996cdb8995d771e0add7fd65e1435ca68222
Amandeep Bindra <159040038+amandeepbindra-com@users.noreply.github.com>
2024-02-23 10:45:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".slide-content", {
+var swiper = new Swiper("#slider1", {
     slidesPerView: 3,
     spaceBetween: 25,
     loop: true,
@@ -11,8 +11,39 @@ var swiper = new Swiper(".slide-content", {
       dynamicBullets: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: "#slider1-next",
+      prevEl: "#slider1-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+
+var swiper = new Swiper("#slider2", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: "#slider2-next",
+      prevEl: "#slider2-prev",
     },
 
     breakpoints:{
